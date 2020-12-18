17fce7ce60938b83787c36564035189a377df40f
Tom Houlé <tom@tomhoule.com>
2020-12-18 10:26:54 +0100

    Further document and simplify

--- src/dev.lean
+++ src/dev.lean
@@ -4,21 +4,39 @@ import data.option.basic
 
 # Model of a `dev` command for the Prisma migration engine
 
-The `dev` command acts as a wrapper around `diagnoseMigrationHistory`. Its role
-is to interpret the diagnostic output, and translate it to a concrete action to
-be performed by the CLI.
+The `dev` RPC command acts as a wrapper around `diagnoseMigrationHistory`. Its
+role is to interpret the diagnostic output, and translate it to a concrete
+action to be performed by the CLI.
 
 The corresponding control flow in the CLI would be:
 
 1. Call `RPC dev`. Check the output:
-  - Error / BrokenMigration -> display the error (regular user-facing error, no CLI code should be needed)
-  - Reset -> Prompt the user to reset with the provided reason. Call `RPC reset`, then proceed with 2.
+  - Error / BrokenMigration -> display the error (regular user-facing error, no
+    CLI code should be needed)
+  - Reset -> Prompt the user to reset with the provided reason. Call `RPC
+    reset`, then proceed with 2.
   - CreateMigration -> proceed with 2.
 2. Call `RPC applyMigrations`
 3. If we have no migration name, prompt for it.
 4. Check for the `--create-only` flag
   - If it was passed, call `RPC createMigration`. Done.
-  - Otherwise, call `RPC evaluateDataLoss`, `RPC createMigration`, `RPC applyMigrations`. Generate the client. Done.
+  - Otherwise, call `RPC evaluateDataLoss`, `RPC createMigration`, `RPC
+    applyMigrations`. Generate the client. Done.
+
+Intended JSON-RPC API:
+
+```typescript
+interface DevInput {}
+
+interface DevOutput {
+  action: DevAction
+}
+
+type DevAction =
+  { tag: "Reset", reason: string }
+  | { tag: "CreateMigration" }
+
+```
 
 -/
 
@@ -27,17 +45,20 @@ open except (error ok)
 variables { α : Type }
 universes u v
 
+/-- The top-level RPC input type. -/
+inductive DevInput : Type
+| mk : DevInput
+
 inductive ResetReason
 | Drifted
 | Unspecified
 
-structure DevInput :=
-mk :: ( name : option string )
-
+/-- The top-level RPC output type. -/
 inductive DevOutput
 | CreateMigration
 | Reset : ResetReason → DevOutput
--- This is a user-facing error output, it does need any special handling in the CLI.
+-- This manifests itself as a user-facing error output, it does need any special
+-- handling in the CLI.
 | BrokenMigration : string -> DevOutput
 
 inductive DriftDiagnostic : Type
@@ -112,6 +133,7 @@ checkBrokenMigration projectState >>
   checkReset projectState >>
   throw DevOutput.CreateMigration
 
+/-- Convenience wrapper around `dev` to make proof types more readable. -/
 def runDev : DevInput → DiagnoseMigrationHistoryOutput → DevOutput :=
 λ input diagnostics, match run (dev input diagnostics) with
 | (error output) := output
@@ -129,10 +151,10 @@ theorem devReset :
   ∀ (input : DevInput) (projectState : DiagnoseMigrationHistoryOutput),
   projectState.brokenMigration = none →
   projectState.resetReason.is_some →
-  ∃ r, run (dev input projectState) = except.error (DevOutput.Reset r) :=
+  ∃ r, runDev input projectState = DevOutput.Reset r :=
 begin
   intros input projectState hBroken hReset,
-  delta dev checkBrokenMigration checkReset,
+  delta runDev dev checkBrokenMigration checkReset,
   obtain ⟨r, hSome⟩ : ∃ r, projectState.resetReason = some r, from option.is_some_iff_exists.mp hReset,
   existsi r,
   simp [hReset],
@@ -141,29 +163,30 @@ begin
 end
 
 /--
-Whenever we are not in a reset situation, we will return `CreateMigration`. -/
+Whenever we are not in a reset situation and no migration is broken, we will
+return `CreateMigration`. -/
 theorem devCreateMigration :
   ∀ (input : DevInput) (projectState : DiagnoseMigrationHistoryOutput),
-  input.name.is_some →
   projectState.resetReason = none →
   projectState.brokenMigration = none →
-  run (dev input projectState) = error DevOutput.CreateMigration :=
+  runDev input projectState = DevOutput.CreateMigration :=
 begin
-  intros input projectState hInputNamePresent hNoReset hNoBrokenMigration,
-  delta dev checkBrokenMigration checkReset,
+  intros input projectState hNoReset hNoBrokenMigration,
+  delta runDev dev checkBrokenMigration checkReset,
   rw [hNoReset, hNoBrokenMigration],
   refl
 end
 
 /--
-`dev` will always return an error before asking for a reset in case a | |
+`dev` will always return an error before asking for a reset in case a
 migration doesn't apply cleanly to the dev database. It never prematurely asks
 for a reset. -/
 theorem devBrokenMigration :
   ∀ (input : DevInput) (projectState : DiagnoseMigrationHistoryOutput) (brokenMigrationName : string),
-  projectState.brokenMigration = some brokenMigrationName → run (dev input projectState) = except.error (DevOutput.BrokenMigration brokenMigrationName) :=
+  projectState.brokenMigration = some brokenMigrationName →
+  runDev input projectState = DevOutput.BrokenMigration brokenMigrationName :=
 begin
   rintros input projectState brokenMigrationName hBroken,
-  unfold dev checkBrokenMigration,
+  unfold runDev dev checkBrokenMigration,
   simpa [hBroken]
 end
